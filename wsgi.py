df2bde343aa092b6721d5743606519ab4ba56f3a
Mehak Imran <mehakimran348@gmail.com>
2026-01-28 16:55:22 +0500

    Add wsgi.py for WSGI deployment

---
+++ wsgi.py
@@ -0,0 +1,10 @@
+import sys
+import os
+
+# Add project directory to sys.path
+project_home = os.path.dirname(os.path.abspath(__file__))
+if project_home not in sys.path:
+    sys.path.insert(0, project_home)
+
+# Import your Flask app
+from app import app as application
